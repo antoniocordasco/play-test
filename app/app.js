0a46c6deaffdf79eefd22aad31dad1279af68358
antoniocordasco <antonio.cordasco@skyscanner.net>
2019-07-28 10:05:27 +0100

    port issues

--- app/app.js
+++ app/app.js
@@ -14,10 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 5000;
-}
+let port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 });
